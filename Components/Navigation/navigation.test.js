8705bf78ed75167774fdd19680f36098dca8f14c
Georgios Alexandros Christodoulou <bab_alt@hotmail.com>
2023-06-06 20:28:30 +0200

    added navigation at bottom of every page and equivalent tests

---
+++ Components/Navigation/navigation.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import Navigation from "./index";
+
+test("There is a link labeled Homepage that directs to the Homepage", () => {
+  render(<Navigation />);
+  const link = screen.getByRole("link", { name: "Homepage" });
+  expect(link).toHaveAttribute("href", "/");
+});
+
+test("There is a link labeled Art Pieces that directs to the Art Pieces page", () => {
+  render(<Navigation />);
+  const link = screen.getByRole("link", { name: "Art Pieces" });
+  expect(link).toHaveAttribute("href", "/art-pieces");
+});
